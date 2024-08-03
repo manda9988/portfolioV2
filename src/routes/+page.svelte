15d9178cfdb2b6340730adb0f0995396538360f5
KAEL <vincent.achy@gmail.com>
2024-08-03 20:31:09 +0200

    commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,27 +5,40 @@
 </script>
 
 <div class="container">
-	<h1>Vincent Achy</h1>
-	<h2>Développeur Front-End | Svelte / React</h2>
+	<div class="title">
+		<h1>Vincent Achy</h1>
+		<h2>Développeur Front-End | Svelte / React</h2>
+	</div>
 
-	<section>
-		<h3>About</h3>
-		<p>
-			Formé à l'école O'Clock et par mes expériences autodidactes et professionnelles, je me
-			consacre au développement web depuis trois ans.
-		</p>
-		<p>
-			J'ai acquis de solides compétences en pratiquant diverses technologies telles que :
-			JavaScript, SCSS, Svelte, React, Node.js, Express et PostgreSQL.
-		</p>
-		<p>
-			Mon parcours professionnel m'a apporté des aptitudes essentielles en communication et en
-			travail d'équipe.
-		</p>
-		<p>
-			Motivé par l'innovation, je suis prêt à m'engager dans des projets constructifs en apportant
-			ma capacité d'adaptation et mon savoir-faire.
-		</p>
+	<!-- Bar Effect -->
+	<article class="bar-effect">
+		<div class="bar bg-blue-950"></div>
+		<div class="bar bg-blue-800"></div>
+		<div class="bar bg-blue-950"></div>
+	</article>
+
+	<section class="about">
+		<h3>
+			<img src="/about.png" alt="Icon" class="icon" /> About
+		</h3>
+		<div class="paragraphe">
+			<p>
+				Formé à l'école O'Clock et par mes expériences autodidactes et professionnelles, je me
+				consacre au développement web depuis trois ans.
+			</p>
+			<p>
+				J'ai acquis de solides compétences en pratiquant diverses technologies telles que :
+				JavaScript, SCSS, Svelte, React, Node.js, Express et PostgreSQL.
+			</p>
+			<p>
+				Mon parcours professionnel m'a apporté des aptitudes essentielles en communication et en
+				travail d'équipe.
+			</p>
+			<p>
+				Motivé par l'innovation, je suis prêt à m'engager dans des projets constructifs en apportant
+				ma capacité d'adaptation et mon savoir-faire.
+			</p>
+		</div>
 	</section>
 
 	<!-- <Projects /> -->
