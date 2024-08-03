7d2a5c6cee3a31c4918246b99acca7269f8ac13b
KAEL <vincent.achy@gmail.com>
2024-08-03 18:37:56 +0200

    Initial commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import '../styles/global.scss';
+	import '../styles/page.scss';
+	import Projects from './Projects.svelte';
+</script>
+
+<div class="container">
+	<h1>Vincent Achy</h1>
+	<h2>Développeur Front-End | Svelte / React</h2>
+
+	<section>
+		<h3>About</h3>
+		<p>
+			Formé à l'école O'Clock et par mes expériences autodidactes et professionnelles, je me
+			consacre au développement web depuis trois ans.
+		</p>
+		<p>
+			J'ai acquis de solides compétences en pratiquant diverses technologies telles que :
+			JavaScript, SCSS, Svelte, React, Node.js, Express et PostgreSQL.
+		</p>
+		<p>
+			Mon parcours professionnel m'a apporté des aptitudes essentielles en communication et en
+			travail d'équipe.
+		</p>
+		<p>
+			Motivé par l'innovation, je suis prêt à m'engager dans des projets constructifs en apportant
+			ma capacité d'adaptation et mon savoir-faire.
+		</p>
+	</section>
+
+	<!-- <Projects /> -->
+</div>
---
+++ src/routes/Projects.svelte
@@ -0,0 +1,17 @@
+<script>
+	import '../styles/projects.scss';
+</script>
+
+<section class="projects">
+	<h3>Projects</h3>
+	<div class="project">
+		<h4>2024 - Blog_</h4>
+		<p>Une application pour la création d'articles généralistes.</p>
+		<p>Technologies : Svelte, Vite, SCSS, PostgreSQL, Node.js, Express</p>
+	</div>
+	<div class="project">
+		<h4>2023 - AMS_Architects</h4>
+		<p>Un site présentant les travaux d'une agence d'architecture.</p>
+		<p>Technologies : Svelte, Vite, SCSS, PostgreSQL, Node.js, Express</p>
+	</div>
+</section>
