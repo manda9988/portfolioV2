a3a621b2fdd08f7c186cd58743200fab2814c7ee
KAEL <vincent.achy@gmail.com>
2024-08-05 21:34:09 +0200

    commit

---
+++ src/routes/Contact.svelte
@@ -0,0 +1,33 @@
+<!-- Contact.svelte -->
+
+<script>
+	import '../styles/contact.scss';
+</script>
+
+<section class="contact">
+	<h3>
+		<span class="icon-wrapper">
+			<img src="/contacts.png" alt="Icon" class="icon" />
+		</span>
+		<span class="text">Contact</span>
+	</h3>
+
+	<div class="contact-items">
+		<div class="contact-item">
+			<p class="email">vincent.achy@gmail.com</p>
+		</div>
+
+		<div class="contact-item">
+			<a href="https://github.com/vincentachy" target="_blank" rel="noopener noreferrer">Github</a>
+		</div>
+		<div class="contact-item">
+			<a href="https://linkedin.com/in/vincentachy" target="_blank" rel="noopener noreferrer"
+				>LinkedIn</a
+			>
+		</div>
+
+		<div class="contact-item">
+			<a href="/path/to/cv.pdf" target="_blank" rel="noopener noreferrer">Curriculum vitæ</a>
+		</div>
+	</div>
+</section>
