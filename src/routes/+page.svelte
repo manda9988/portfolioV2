4e4b09401a9e9dc9913129287d1d7192d2ddae99
KAEL <vincent.achy@gmail.com>
2024-08-05 16:16:31 +0200

    commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import '../styles/global.scss';
 	import '../styles/page.scss';
 	import Projects from './Projects.svelte';
+	import About from './About.svelte';
 </script>
 
 <div class="container">
@@ -12,33 +13,8 @@
 
 	<div class="bar-one"></div>
 
-	<section class="about">
-		<h3>
-			<span class="icon-wrapper">
-				<img src="/about.png" alt="Icon" class="icon" />
-			</span>
-			<span class="text">About</span>
-		</h3>
-		<div class="paragraphe">
-			<p>
-				Formé à l'école O'Clock et par mes expériences autodidactes et professionnelles, je me
-				consacre au développement web depuis trois ans.
-			</p>
-			<p>
-				J'ai acquis de solides compétences en pratiquant diverses technologies telles que : <span
-					class="highlight">JavaScript, SCSS, Svelte, React, Node.js, Express et PostgreSQL</span
-				>.
-			</p>
-			<p>
-				Mon parcours professionnel m'a apporté des aptitudes essentielles en communication et en
-				travail d'équipe.
-			</p>
-			<p>
-				Motivé par l'innovation, je suis prêt à m'engager dans des projets constructifs en apportant
-				ma capacité d'adaptation et mon savoir-faire.
-			</p>
-		</div>
-	</section>
+	<About />
+
 	<div class="bar-two"></div>
 
 	<!-- <Projects /> -->
---
+++ src/routes/About.svelte
@@ -0,0 +1,31 @@
+<script>
+	import '../styles/about.scss';
+</script>
+
+<section class="about">
+	<h3>
+		<span class="icon-wrapper">
+			<img src="/about.png" alt="Icon" class="icon" />
+		</span>
+		<span class="text">About</span>
+	</h3>
+	<div class="paragraphe">
+		<p>
+			Formé à l'école O'Clock et par mes expériences autodidactes et professionnelles, je me
+			consacre au développement web depuis trois ans.
+		</p>
+		<p>
+			J'ai acquis de solides compétences en pratiquant diverses technologies telles que :
+			<span class="highlight">JavaScript, SCSS, Svelte, React, Node.js, Express et PostgreSQL</span
+			>.
+		</p>
+		<p>
+			Mon parcours professionnel m'a apporté des aptitudes essentielles en communication et en
+			travail d'équipe.
+		</p>
+		<p>
+			Motivé par l'innovation, je suis prêt à m'engager dans des projets constructifs en apportant
+			ma capacité d'adaptation et mon savoir-faire.
+		</p>
+	</div>
+</section>
